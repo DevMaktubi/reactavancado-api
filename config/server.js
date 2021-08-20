12e404114d8637c57400fceb6af917fdd5c13969
DevMaktubi <brenoluiz1@gmail.com>
2021-08-20 12:28:46 -0300

    First commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ec970362235e8cdb97c42d2aa72bad75'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '36a68fd5-1b69-4b96-9d4b-d959e6cd1f5f'
+};
